0ce83febe8106ba688a7e493e1d20b9f19226358
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2022-10-15 19:03:01 +0200

    Add project files.

---
+++ AOQBIY_HFT_2022231.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AOQBIY_HFT_2022231.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ AOQBIY_HFT_2022231.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AOQBIY_HFT_2022231.Repository
+{
+    public class Class1
+    {
+    }
+}
