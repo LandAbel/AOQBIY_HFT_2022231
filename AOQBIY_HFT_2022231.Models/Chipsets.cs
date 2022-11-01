8871c6ec48f8421b4db263a4a859f845bf55aaf6
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2022-11-01 12:40:34 +0100

    Chipset added

---
+++ AOQBIY_HFT_2022231.Models/Chipsets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOQBIY_HFT_2022231.Models
+{
+    [Table("Chipset")]
+    public class Chipsets
+    {
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(Processor))]
+        public int ProcessorId { get; set; }
+
+
+    }
+}
