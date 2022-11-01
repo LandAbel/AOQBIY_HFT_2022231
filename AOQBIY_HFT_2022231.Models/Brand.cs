77a476195c79926bc389ad5dd07d813db7c180d1
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2022-11-01 12:40:22 +0100

    Brand class added

---
+++ AOQBIY_HFT_2022231.Models/Brand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.ConstrainedExecution;
+
+namespace AOQBIY_HFT_2022231.Models
+{
+    [Table("Brands")]
+    public class Brand
+    {
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Processor> Processors { get; set; }
+
+        public Brand()
+        {
+            this.Processors = new HashSet<Processor>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
