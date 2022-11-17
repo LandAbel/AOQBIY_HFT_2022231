bd144d469a95cdad2f033051a2565db678b023e9
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2022-11-17 20:06:09 +0100

    Processor Repository added

---
+++ AOQBIY_HFT_2022231.Repository/Repos/ProcessorRepository.cs
@@ -0,0 +1,25 @@
+﻿using AOQBIY_HFT_2022231.Models;
+using AOQBIY_HFT_2022231.Repository.Data;
+using AOQBIY_HFT_2022231.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOQBIY_HFT_2022231.Repository.Repos
+{
+    public class ProcessorRepository:Repository<Processor>, IProcessorRepository
+    {
+        public ProcessorRepository(ProcessorListDbContext ctx) : base(ctx)
+        {
+        }
+        public void UpdateMaxTurboFrequency(int id, int newMaxFreq)
+        {
+            Processor old = Read(id);
+            old.MaxTurboFrequency = newMaxFreq;
+            ctx.SaveChanges();
+        }
+    }
+}
