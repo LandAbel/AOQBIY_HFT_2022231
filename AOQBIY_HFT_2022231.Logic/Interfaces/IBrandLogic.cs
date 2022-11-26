7a5327fcc08c328c7049520bcf80a4fb5588995b
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2022-11-26 20:43:42 +0100

    Brand Interface generated

---
+++ AOQBIY_HFT_2022231.Logic/Interfaces/IBrandLogic.cs
@@ -0,0 +1,15 @@
+﻿using AOQBIY_HFT_2022231.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOQBIY_HFT_2022231.Logic.Interfaces
+{
+    public interface IBrandLogic
+    {
+        void Create(Brand item);
+        void Delete(int id);
+        Brand Read(int id);
+        IEnumerable<Brand> ReadAll();
+        void Update(Brand item);
+    }
+}
