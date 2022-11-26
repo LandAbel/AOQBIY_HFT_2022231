9d23594bf018e04643dd1dedefd18c44eddb0e9a
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2022-11-26 20:43:55 +0100

    Chipset Interface generated

---
+++ AOQBIY_HFT_2022231.Logic/Interfaces/IChipsetLogic.cs
@@ -0,0 +1,15 @@
+﻿using AOQBIY_HFT_2022231.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOQBIY_HFT_2022231.Logic.Interfaces
+{
+    public interface IChipsetLogic
+    {
+        void Create(Chipsets item);
+        void Delete(int id);
+        Chipsets Read(int id);
+        IEnumerable<Chipsets> ReadAll();
+        void Update(Chipsets item);
+    }
+}
