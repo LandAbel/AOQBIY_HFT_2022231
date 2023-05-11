8cb7c0a112ed8b6f0fa75cff1e55e6c48d3ab3be
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2023-05-12 00:20:46 +0200

    SignalR hub created

---
+++ AOQBIY_HFT_2022231.Endpoint/Services/SignalRHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace AOQBIY_HFT_2022231.Endpoint.Services
+{
+    public class SignalRHub:Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.Caller.SendAsync("Conected", Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.Caller.SendAsync("Disconnected", Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
