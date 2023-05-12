d4194d7e9e4a8d70382bcb96fbe763d84b2e27f3
LandAbel <Landenberger_abel@stud.uni-obuda.hu>
2023-05-13 01:29:36 +0200

    Design and script added to main page

---
+++ AOQBIY_HFT_2022231.WebClient/wwwroot/indexScript.js
@@ -0,0 +1,12 @@
+﻿function loadBrandPage() {
+    window.location = "base.html";
+}
+function loadChipsetPage() {
+    window.location = "chipsetpage.html";
+}
+function loadProcessorPage(){
+    window.location = "processorpage.html";
+}
+function loadStatisticsPage(){
+    window.location = "noncrud.html";
+}
